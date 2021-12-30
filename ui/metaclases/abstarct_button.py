786b8ccb70aacc1fa47b309163417ebeed554699
Nermitor <dmatyukhin2006@mail.ru>
2021-12-30 17:17:59 +0700

    Улучшен код кнопок

--- ui/metaclases/abstarct_button.py
+++ ui/metaclases/abstarct_button.py
@@ -5,6 +5,7 @@ class AbstractButton(pg.sprite.Sprite):
     def __init__(self, x: int, y: int, call_back_function: callable):
         super().__init__()
         self.call_back_function = call_back_function
+        self.rect = self.image.get_rect()
         self.rect.x = x - self.rect.width // 2
         self.rect.y = y
 
