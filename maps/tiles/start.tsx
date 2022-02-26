b60d7cf0d26fbea966fef255bb5a645d43581dcd
Nermitor <dmatyukhin2006@mail.ru>
2022-02-26 22:33:19 +0700

    fix

--- maps/tiles/start.tsx
+++ maps/tiles/start.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="start" tilewidth="20" tileheight="14" tilecount="1" columns="1">
- <image source="../../../../../Downloads/pixil-frame-0.png" width="20" height="14"/>
+ <image source="pixil-frame-0.png" width="20" height="14"/>
 </tileset>
